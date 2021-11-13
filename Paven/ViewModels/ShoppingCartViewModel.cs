10558de9872d2149513805f2041959d2bc8cecf6
Atish Krishen Hariparsad <21825470@dut4life.ac.za>
2021-11-13 18:16:07 +0200

    ViewModels

---
+++ Paven/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Paven.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Paven.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        [Key]
+        public List<Cart> CartItems { get; set; }
+        public double CartTotal { get; set; }
+    }
+}
