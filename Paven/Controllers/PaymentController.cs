d0dc3b8dafd2ecba7709cea48e1cb4221b43cffe
Atish Krishen Hariparsad <21825470@dut4life.ac.za>
2021-11-21 16:56:27 +0200

    Published

---
+++ Paven/Controllers/PaymentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paven.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Payment/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Payment/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Payment - Success
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+        // GET: Payment - Success
+        public ActionResult Cancelled()
+        {
+            return View();
+        }
+    }
+}
